2a3613ed9db07c8876b07e0a58ebd3e94bce7597
bohdan-ryzhko <m3meizumini@gmail.com>
2025-06-16 16:30:32 +0300

    adjusted header actions styles

--- src/components/header/parts/actions.tsx
+++ src/components/header/parts/actions.tsx
@@ -3,18 +3,22 @@ import { BellRing } from 'lucide-react';
 
 export function Actions() {
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center gap-1.5">
       <Button variant="secondary" className="!max-h-8 px-3.5">
         <span className="body-2 font-semibold text-charcoal">
           Account plans
         </span>
       </Button>
 
-      <Button variant="ghost" className="hover:bg-gray-foreground">
+      <Button
+        variant="ghost"
+        className="hover:bg-gray-foreground !px-1 !max-h-8 ml-2">
         <BellRing color="white" className="min-w-5 min-h-5 max-w-5 max-h-5" />
       </Button>
 
-      <Button variant="ghost" className="hover:bg-gray-foreground">
+      <Button
+        variant="ghost"
+        className="px-2 hover:bg-gray-foreground !max-h-8">
         <span className="body-2 text-white">Support</span>
       </Button>
 
